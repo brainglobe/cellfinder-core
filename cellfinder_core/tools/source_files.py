48d301630b47d793a9abd204440b4d2ddbf88de5
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-03-10 17:57:19 +0000

    update sourcing

--- cellfinder_core/tools/source_files.py
+++ cellfinder_core/tools/source_files.py
@@ -2,8 +2,8 @@ from pkg_resources import resource_filename
 
 
 def source_config_cellfinder():
-    return resource_filename("cellfinder", "config/cellfinder.conf")
+    return resource_filename("cellfinder_core", "config/cellfinder.conf")
 
 
 def source_custom_config_cellfinder():
-    return resource_filename("cellfinder", "config/cellfinder.conf.custom")
+    return resource_filename("cellfinder_core", "config/cellfinder.conf.custom")
