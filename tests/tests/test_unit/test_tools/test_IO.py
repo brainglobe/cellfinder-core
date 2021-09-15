7a86a7d2c879c94da529ec6140f7e5c3f02bf288
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2021-09-15 18:40:12 +0200

    fixed side effect with tests searching for subdirectories moved brain_paths.txt to brain directory replaced paths with constants

--- tests/tests/test_unit/test_tools/test_IO.py
+++ tests/tests/test_unit/test_tools/test_IO.py
@@ -2,14 +2,17 @@ import dask.array as d_array
 
 from cellfinder_core.tools import IO
 
+BRAIN_DIR = "tests/data/brain"
+BRAIN_PATHS = f"{BRAIN_DIR}/brain_paths.txt"
+
 
 def test_read_with_dask_txt():
-    stack = IO.read_with_dask("tests/data/tools/brain_paths.txt")
+    stack = IO.read_with_dask(BRAIN_PATHS)
     assert type(stack) == d_array.Array
 
 
 def test_read_with_dask_glob_txt_equal():
-    txt_stack = IO.read_with_dask("tests/data/tools/brain_paths.txt")
-    glob_stack = IO.read_with_dask("tests/data/brain/")
+    txt_stack = IO.read_with_dask(BRAIN_PATHS)
+    glob_stack = IO.read_with_dask(BRAIN_DIR)
 
     assert d_array.equal(txt_stack, glob_stack).all()
