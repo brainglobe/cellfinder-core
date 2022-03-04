cd27df2c6d3541a62bb6486ac51398eecf277061
David Stansby <dstansby@gmail.com>
2022-03-04 14:25:40 +0000

    Refactor to classes

--- cellfinder_core/detect/detect.py
+++ cellfinder_core/detect/detect.py
@@ -38,128 +38,157 @@ def calculate_parameters_in_pixels(
     return soma_diameter, max_cluster_size, ball_xy_size, ball_z_size
 
 
-def main(
-    signal_array,
-    start_plane,
-    end_plane,
-    voxel_sizes,
-    soma_diameter,
-    max_cluster_size,
-    ball_xy_size,
-    ball_z_size,
-    ball_overlap_fraction,
-    soma_spread_factor,
-    n_free_cpus,
-    log_sigma_size,
-    n_sds_above_mean_thresh,
-    outlier_keep=False,
-    artifact_keep=False,
-    save_planes=False,
-    plane_directory=None,
-    verbose=True,
-):
-    """
-    Parameters
-    ----------
-    verbose : bool
-        If ``True`` print progress and show progress bars to the terminal.
-    """
-    n_processes = get_num_processes(min_free_cpu_cores=n_free_cpus)
-    start_time = datetime.now()
+class MainRunner:
+    def __init__(self):
+        """
+        Attributes
+        ----------
+        mp3d_filter_output_queue : multiprocessing.Queue
+        bf_process : multiprocessing.Process
+        """
+        self.mp3d_filter_output_queue = MultiprocessingQueue()
+        self.processes = []
+        self.mp_3d_filter_queue = None
 
-    (
-        soma_diameter,
-        max_cluster_size,
-        ball_xy_size,
-        ball_z_size,
-    ) = calculate_parameters_in_pixels(
+    def run(self, *args, **kwargs):
+        self.start(*args, **kwargs)
+        return self.finish()
+
+    def start(
+        self,
+        signal_array,
+        start_plane,
+        end_plane,
         voxel_sizes,
         soma_diameter,
         max_cluster_size,
         ball_xy_size,
         ball_z_size,
-    )
-
-    if end_plane == -1:
-        end_plane = len(signal_array)
-    signal_array = signal_array[start_plane:end_plane]
-
-    workers_queue = MultiprocessingQueue(maxsize=n_processes)
-    # WARNING: needs to be AT LEAST ball_z_size
-    mp_3d_filter_queue = MultiprocessingQueue(maxsize=ball_z_size)
-    for plane_id in range(n_processes):
-        # place holder for the queue to have the right size on first run
-        workers_queue.put(None)
-
-    if signal_array.ndim != 3:
-        raise IOError("Input data must be 3D")
-
-    setup_params = [
-        signal_array[0, :, :],
-        soma_diameter,
-        ball_xy_size,
-        ball_z_size,
         ball_overlap_fraction,
-        start_plane,
-    ]
-    output_queue = MultiprocessingQueue()
+        soma_spread_factor,
+        n_free_cpus,
+        log_sigma_size,
+        n_sds_above_mean_thresh,
+        outlier_keep=False,
+        artifact_keep=False,
+        save_planes=False,
+        plane_directory=None,
+        verbose=True,
+    ):
+        """
+        Start running the detection algorithm.
 
-    mp_3d_filter = Mp3DFilter(
-        mp_3d_filter_queue,
-        output_queue,
-        soma_diameter,
-        setup_params=setup_params,
-        soma_size_spread_factor=soma_spread_factor,
-        planes_paths_range=signal_array,
-        save_planes=save_planes,
-        plane_directory=plane_directory,
-        start_plane=start_plane,
-        max_cluster_size=max_cluster_size,
-        outlier_keep=outlier_keep,
-        artifact_keep=artifact_keep,
-        verbose=verbose,
-    )
+        Parameters
+        ----------
+        verbose : bool
+            If ``True`` print progress and show progress bars to the terminal.
+        """
+        self.verbose = verbose
+        n_processes = get_num_processes(min_free_cpu_cores=n_free_cpus)
+        self.start_time = datetime.now()
 
-    # start 3D analysis (waits for planes in queue)
-    bf_process = multiprocessing.Process(target=mp_3d_filter.process, args=())
-    bf_process.start()  # needs to be started before the loop
-    clipping_val, threshold_value = setup_tile_filtering(signal_array[0, :, :])
-    mp_tile_processor = MpTileProcessor(workers_queue, mp_3d_filter_queue)
-    prev_lock = Lock()
-    processes = []
-
-    # start 2D tile filter (output goes into queue for 3D analysis)
-    for plane_id, plane in enumerate(signal_array):
-        workers_queue.get()
-        lock = Lock()
-        lock.acquire()
-        p = multiprocessing.Process(
-            target=mp_tile_processor.process,
-            args=(
-                plane_id,
-                np.array(plane),
-                prev_lock,
-                lock,
-                clipping_val,
-                threshold_value,
-                soma_diameter,
-                log_sigma_size,
-                n_sds_above_mean_thresh,
-            ),
+        (
+            soma_diameter,
+            max_cluster_size,
+            ball_xy_size,
+            ball_z_size,
+        ) = calculate_parameters_in_pixels(
+            voxel_sizes,
+            soma_diameter,
+            max_cluster_size,
+            ball_xy_size,
+            ball_z_size,
         )
-        prev_lock = lock
-        processes.append(p)
-        p.start()
 
-    processes[-1].join()
-    mp_3d_filter_queue.put((None, None, None))  # Signal the end
-    cells = output_queue.get()
-    bf_process.join()
+        if end_plane == -1:
+            end_plane = len(signal_array)
+        signal_array = signal_array[start_plane:end_plane]
 
-    if verbose:
-        print(
-            "Detection complete - all planes done in : {}".format(
-                datetime.now() - start_time
+        workers_queue = MultiprocessingQueue(maxsize=n_processes)
+        # WARNING: needs to be AT LEAST ball_z_size
+        self.mp_3d_filter_queue = MultiprocessingQueue(maxsize=ball_z_size)
+        for plane_id in range(n_processes):
+            # place holder for the queue to have the right size on first run
+            workers_queue.put(None)
+
+        if signal_array.ndim != 3:
+            raise IOError("Input data must be 3D")
+
+        setup_params = [
+            signal_array[0, :, :],
+            soma_diameter,
+            ball_xy_size,
+            ball_z_size,
+            ball_overlap_fraction,
+            start_plane,
+        ]
+
+        mp_3d_filter = Mp3DFilter(
+            self.mp_3d_filter_queue,
+            self.mp3d_filter_output_queue,
+            soma_diameter,
+            setup_params=setup_params,
+            soma_size_spread_factor=soma_spread_factor,
+            planes_paths_range=signal_array,
+            save_planes=save_planes,
+            plane_directory=plane_directory,
+            start_plane=start_plane,
+            max_cluster_size=max_cluster_size,
+            outlier_keep=outlier_keep,
+            artifact_keep=artifact_keep,
+            verbose=verbose,
+        )
+
+        # start 3D analysis (waits for planes in queue)
+        self.bf_process = multiprocessing.Process(
+            target=mp_3d_filter.process, args=()
+        )
+        self.bf_process.start()  # needs to be started before the loop
+        clipping_val, threshold_value = setup_tile_filtering(
+            signal_array[0, :, :]
+        )
+        mp_tile_processor = MpTileProcessor(
+            workers_queue, self.mp_3d_filter_queue
+        )
+        prev_lock = Lock()
+
+        # start 2D tile filter (output goes into queue for 3D analysis)
+        for plane_id, plane in enumerate(signal_array):
+            workers_queue.get()
+            lock = Lock()
+            lock.acquire()
+            p = multiprocessing.Process(
+                target=mp_tile_processor.process,
+                args=(
+                    plane_id,
+                    np.array(plane),
+                    prev_lock,
+                    lock,
+                    clipping_val,
+                    threshold_value,
+                    soma_diameter,
+                    log_sigma_size,
+                    n_sds_above_mean_thresh,
+                ),
             )
-        )
-    return cells
+            prev_lock = lock
+            self.processes.append(p)
+            p.start()
+
+    def finish(self):
+        self.processes[-1].join()
+        self.mp_3d_filter_queue.put((None, None, None))  # Signal the end
+        cells = self.mp3d_filter_output_queue.get()
+        self.bf_process.join()
+
+        if self.verbose:
+            print(
+                "Detection complete - all planes done in : {}".format(
+                    datetime.now() - self.start_time
+                )
+            )
+        return cells
+
+
+def main(*args, **kwargs):
+    return MainRunner().run(*args, **kwargs)
--- cellfinder_core/main.py
+++ cellfinder_core/main.py
@@ -13,87 +13,98 @@ tf_suppress_log_messages = [
 ]
 
 
-def main(
-    signal_array,
-    background_array,
-    voxel_sizes,
-    start_plane=0,
-    end_plane=-1,
-    trained_model=None,
-    model_weights=None,
-    model="resnet50_tv",
-    batch_size=32,
-    n_free_cpus=2,
-    network_voxel_sizes=[5, 1, 1],
-    soma_diameter=16,
-    ball_xy_size=6,
-    ball_z_size=15,
-    ball_overlap_fraction=0.6,
-    log_sigma_size=0.2,
-    n_sds_above_mean_thresh=10,
-    soma_spread_factor=1.4,
-    max_cluster_size=100000,
-    cube_width=50,
-    cube_height=50,
-    cube_depth=20,
-    network_depth="50",
-    verbose=True,
-):
-    suppress_tf_logging(tf_suppress_log_messages)
+class MainRunner:
+    def __init__(self):
+        from cellfinder_core.detect import MainRunner as DetectRunner
 
-    from pathlib import Path
+        self.detect_runner = DetectRunner()
 
-    from cellfinder_core.classify import classify
-    from cellfinder_core.detect import detect
-    from cellfinder_core.tools import prep
-
-    home = Path.home()
-    install_path = home / ".cellfinder"
-    logging.info("Detecting cell candidates")
-
-    points = detect.main(
+    def __call__(
+        self,
         signal_array,
-        start_plane,
-        end_plane,
+        background_array,
         voxel_sizes,
-        soma_diameter,
-        max_cluster_size,
-        ball_xy_size,
-        ball_z_size,
-        ball_overlap_fraction,
-        soma_spread_factor,
-        n_free_cpus,
-        log_sigma_size,
-        n_sds_above_mean_thresh,
-        verbose=verbose,
-    )
+        start_plane=0,
+        end_plane=-1,
+        trained_model=None,
+        model_weights=None,
+        model="resnet50_tv",
+        batch_size=32,
+        n_free_cpus=2,
+        network_voxel_sizes=[5, 1, 1],
+        soma_diameter=16,
+        ball_xy_size=6,
+        ball_z_size=15,
+        ball_overlap_fraction=0.6,
+        log_sigma_size=0.2,
+        n_sds_above_mean_thresh=10,
+        soma_spread_factor=1.4,
+        max_cluster_size=100000,
+        cube_width=50,
+        cube_height=50,
+        cube_depth=20,
+        network_depth="50",
+        verbose=True,
+    ):
+        suppress_tf_logging(tf_suppress_log_messages)
 
-    model_weights = prep.prep_classification(
-        trained_model, model_weights, install_path, model, n_free_cpus
-    )
-    if len(points) > 0:
-        logging.info("Running classification")
-        points = classify.main(
-            points,
+        from pathlib import Path
+
+        from cellfinder_core.classify import classify
+        from cellfinder_core.detect import detect
+        from cellfinder_core.tools import prep
+
+        home = Path.home()
+        install_path = home / ".cellfinder"
+        logging.info("Detecting cell candidates")
+
+        self.detect_runner.start(
             signal_array,
-            background_array,
-            n_free_cpus,
+            start_plane,
+            end_plane,
             voxel_sizes,
-            network_voxel_sizes,
-            batch_size,
-            cube_height,
-            cube_width,
-            cube_depth,
-            trained_model,
-            model_weights,
-            network_depth,
+            soma_diameter,
+            max_cluster_size,
+            ball_xy_size,
+            ball_z_size,
+            ball_overlap_fraction,
+            soma_spread_factor,
+            n_free_cpus,
+            log_sigma_size,
+            n_sds_above_mean_thresh,
             verbose=verbose,
         )
-    else:
-        logging.info("No candidates, skipping classification")
-    return points
-    # logging.info("Saving classified cells")
-    # save_cells(points, classified_points_path)
+        points = self.detect_runner.finish()
+
+        model_weights = prep.prep_classification(
+            trained_model, model_weights, install_path, model, n_free_cpus
+        )
+        if len(points) > 0:
+            logging.info("Running classification")
+            points = classify.main(
+                points,
+                signal_array,
+                background_array,
+                n_free_cpus,
+                voxel_sizes,
+                network_voxel_sizes,
+                batch_size,
+                cube_height,
+                cube_width,
+                cube_depth,
+                trained_model,
+                model_weights,
+                network_depth,
+                verbose=verbose,
+            )
+        else:
+            logging.info("No candidates, skipping classification")
+        return points
+        # logging.info("Saving classified cells")
+        # save_cells(points, classified_points_path)
+
+
+main = MainRunner()
 
 
 def suppress_tf_logging(tf_suppress_log_messages):
