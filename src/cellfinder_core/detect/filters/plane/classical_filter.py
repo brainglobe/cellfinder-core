cf8be6228af1e6d49a043b63cc73741b0611678c
David Stansby <dstansby@gmail.com>
2023-04-24 11:42:43 +0100

    2D filter images in-place

--- src/cellfinder_core/detect/filters/plane/classical_filter.py
+++ src/cellfinder_core/detect/filters/plane/classical_filter.py
@@ -4,12 +4,18 @@ from scipy.signal import medfilt2d
 
 
 def enhance_peaks(img, clipping_value, gaussian_sigma=2.5):
+    """
+    This function allocates two new copies of `img` with a `float64` dtype
+    to do the filtering.
+    """
     type_in = img.dtype
-    filtered_img = medfilt2d(img.astype(np.float64))
-    filtered_img = gaussian_filter(filtered_img, gaussian_sigma)
-    filtered_img = laplace(filtered_img)
-    filtered_img *= -1
+    filtered_img = img.astype(np.float64)
+    filtered_img = medfilt2d(filtered_img)
+    # Doing these in place prevents allocating new memory
+    gaussian_filter(filtered_img, gaussian_sigma, output=filtered_img)
+    laplace(filtered_img, output=filtered_img)
 
+    filtered_img *= -1
     filtered_img -= filtered_img.min()
     filtered_img /= filtered_img.max()
 
