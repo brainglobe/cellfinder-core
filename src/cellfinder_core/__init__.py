8bfc349316c1722ba39fadd1aea756617521d88e
willGraham01 <1willgraham@gmail.com>
2023-06-21 16:12:53 +0100

    pre-commit run

--- src/cellfinder_core/__init__.py
+++ src/cellfinder_core/__init__.py
@@ -5,7 +5,9 @@ try:
 except PackageNotFoundError:
     # Implying that the package we've just imported doesn't exist.
     # Worrying, but throw an error here in this case
-    raise PackageNotFoundError("cellfinder-core name package is not present, but you appear to be importing it?")
+    raise PackageNotFoundError(
+        "cellfinder-core name package is not present, but you appear to be importing it?"
+    )
 
 __author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
 __license__ = "BSD-3-Clause"
