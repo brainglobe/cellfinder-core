4573a0756faa31ed20dbf1a8f086a0b51b8a75cd
willGraham01 <1willgraham@gmail.com>
2023-06-21 16:06:13 +0100

    Fix dynamic versioning

--- src/cellfinder_core/__init__.py
+++ src/cellfinder_core/__init__.py
@@ -1,4 +1,12 @@
-__version__ = "0.3.1"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("cellfinder-core")
+except PackageNotFoundError:
+    # Implying that the package we've just imported doesn't exist.
+    # Worrying, but throw an error here in this case
+    raise PackageNotFoundError("cellfinder-core name package is not present, but you appear to be importing it?")
+
 __author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
 __license__ = "BSD-3-Clause"
 
