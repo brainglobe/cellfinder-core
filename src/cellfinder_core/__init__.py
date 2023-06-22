e843ca66311db5c51b49db2a7e524ad0ce68c861
Will Graham <32364977+willGraham01@users.noreply.github.com>
2023-06-22 09:17:54 +0100

    Apply suggestions from code review
    Co-authored-by: David Stansby <dstansby@gmail.com>

--- src/cellfinder_core/__init__.py
+++ src/cellfinder_core/__init__.py
@@ -2,12 +2,10 @@ from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("cellfinder-core")
-except PackageNotFoundError:
-    # Implying that the package we've just imported doesn't exist.
-    # Worrying, but throw an error here in this case
+except PackageNotFoundError as e:
     raise PackageNotFoundError(
-        "cellfinder-core name package is not present, but you appear to be importing it?"
-    )
+        "cellfinder-core package not installed"
+    ) from e
 
 __author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
 __license__ = "BSD-3-Clause"
