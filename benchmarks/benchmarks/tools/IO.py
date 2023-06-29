a2b200e9975a2f90f7d038a0832e39ccff4f0583
sfmig <33267254+sfmig@users.noreply.github.com>
2023-06-29 14:44:46 +0100

    remove voxel_size from IO and refactor. change precommit config to skip mypy errors in IO benchmarks

--- benchmarks/benchmarks/tools/IO.py
+++ benchmarks/benchmarks/tools/IO.py
@@ -5,35 +5,20 @@ from cellfinder_core.tools.IO import get_tiff_meta, read_with_dask
 
 p = Path(os.path.dirname(__file__)).absolute()
 CELLFINDER_CORE_PATH = p.parents[2]
-# Q for review: is there a nice way to get cellfinder-core path?
 TESTS_DATA_INTEGRATION_PATH = (
     Path(CELLFINDER_CORE_PATH) / "tests" / "data" / "integration"
 )
+# Q for review: is there a nice way to get cellfinder-core path?
 
 
-# ---------------------------------------------
-# Benchmarks for reading 3d arrays with dask
-# --------------------------------------------
-class Dask:
-    param_names = [
-        "tests_data_integration_subdir",
-        "voxel_sizes",  # in microns
-    ]
-
-    params = (
-        [
-            TESTS_DATA_INTEGRATION_PATH
-            / Path("detection", "crop_planes", "ch0"),
-            TESTS_DATA_INTEGRATION_PATH
-            / Path("detection", "crop_planes", "ch1"),
-        ],
-        [[3, 2, 2], [5, 2, 2]],
-    )
-
-    def setup(self, subdir, voxel_sizes):
+class Read:
+    # ---------------------------------------------
+    # Setup & teardown functions
+    # --------------------------------------------
+    def setup(self, subdir):
         self.data_dir = str(subdir)
 
-    def teardown(self, subdir, voxel_sizes):
+    def teardown(self, subdir):
         del self.data_dir
         # Q for review: do I need this? only if it is the parameter we sweep
         # across?
@@ -41,19 +26,38 @@ class Dask:
         # 8758dabf84001903ea00c31a001809708969a3e4/benchmarks/cosmology.py#L24
         # (they only use teardown in that case)
 
-    def time_read_with_dask(self, subdir, voxel_sizes):
+    # ---------------------------------------------
+    # Benchmarks for reading 3d arrays with dask
+    # --------------------------------------------
+    def time_read_with_dask(self, subdir):
         read_with_dask(self.data_dir)
 
+    time_read_with_dask.param_names = [
+        "tests_data_integration_subdir",
+    ]
+    time_read_with_dask.params = (
+        [
+            TESTS_DATA_INTEGRATION_PATH
+            / Path("detection", "crop_planes", "ch0"),
+            TESTS_DATA_INTEGRATION_PATH
+            / Path("detection", "crop_planes", "ch1"),
+        ],
+    )
 
-# -----------------------------------------------
-# Benchmarks for reading metadata from tif files
-# -------------------------------------------------
-class Tif:
-    param_names = [
+    # -----------------------------------------------
+    # Benchmarks for reading metadata from tif files
+    # -------------------------------------------------
+    def time_get_tiff_meta(
+        self,
+        subdir,
+    ):
+        get_tiff_meta(self.data_dir)
+
+    time_get_tiff_meta.param_names = [
         "tests_data_integration_tiffile",
     ]
 
-    params = (
+    time_get_tiff_meta.params = (
         [
             *[
                 x
@@ -69,15 +73,3 @@ class Tif:
             ],
         ],
     )
-
-    def setup(self, subdir):
-        self.data_dir = str(subdir)
-
-    def teardown(self, subdir):
-        del self.data_dir
-
-    def time_get_tiff_meta(
-        self,
-        subdir,
-    ):
-        get_tiff_meta(self.data_dir)
