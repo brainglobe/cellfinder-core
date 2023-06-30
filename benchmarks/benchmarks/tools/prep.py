febb77630f2868aecdac010ccd555b5975dd4782
sfmig <33267254+sfmig@users.noreply.github.com>
2023-06-30 12:04:49 +0100

    add teardown function to prep benchmarks

--- benchmarks/benchmarks/tools/prep.py
+++ benchmarks/benchmarks/tools/prep.py
@@ -12,81 +12,70 @@ from cellfinder_core.tools.prep import (
 )
 
 
-class Prep:
-    # common params?
-    # rounds = 2  # default
-    # repeat = 1  # default if rounds!= 1
-    # ---> (min_repeat, max_repeat, max_time) = (1, 5, 10.0)
-    # number = 1  # run only once per repeat?
+class PrepModels:
+    param_names = ["model_name"]
+    params = ["resnet50_tv", "resnet50_all"]
 
-    # common setup
-    def setup(self):
-        # print('setup')
+    # increase default timeout to allow for download
+    timeout = 240
 
-        # TODO: how is n_free_cpus and n_process diff?
-        # n_processes: n of CPUs to use?
-        # n_free_cpus: n of CPUs to leave free in the machine?
-        # Determine how many CPU cores to use, based on a minimum number
-        # of cpu cores
-        # to leave free, and an optional max number of processes.
-        self.n_free_cpus = 2  # should this be parametrised??
+    # Q for review:
+    # - should I run only one sample ('number'=1)?
+    #   'number' as defined here:
+    #    https://asv.readthedocs.io/en/stable/writing_benchmarks.html#timing
+    # - how are prep_classification and prep_training different?
+
+    def setup(self, model_name):
+        self.n_free_cpus = 2  # TODO: should this be parametrised??
         self.n_processes = get_num_processes(
             min_free_cpu_cores=self.n_free_cpus
         )
         self.trained_model = None
         self.model_weights = None
-        self.install_path = Path.home() / ".cellfinder"  # default
-        self.model_name = "resnet50_tv"  # resnet50_all
+        self.install_path = Path.home() / ".cellfinder"
+        self.model_name = model_name
 
-    def teardown(self):
-        # pass
+        # remove .cellfinder dir if it exists already
+        shutil.rmtree(self.install_path, ignore_errors=True)
+        # Q for review:
+        # - is this safe?
+        # - should I check if install_path is the expected path?
 
-        # ------------------
-        # TODO: not sure why the benchmark is timing out with this teardown?
-        # remove everything in temp dir?
-        # Q for review: is this safe?
+    def teardown(self, model_name):
+        # remove .cellfinder dir after benchmarks
         shutil.rmtree(self.install_path)
-        # print('teardown')
-        # ------------------
 
-        # print([f for f in Path(self.install_path).glob("*")])
-
-        # for f in Path(self.install_path).glob("*"):
-        #     if f.is_dir():
-        #         shutil.rmtree(f,)
-        # return
-        # #     print(f)
-        #     f.unlink(missing_ok=True)
-        # Path(self.install_path).rmdir()
-
-    def time_prep_tensorfow(self):
-        prep_tensorflow(self.n_processes)
-
-    def time_prep_models(self):
-        # downloads model weights to .cellfinder dir
-        # -should I remove the files after each rep so that it is comparable?
-        # (overwriting may not be the same as writing from scratch?)
+    def time_prep_models(self, model_name):
         prep_models(
             self.trained_model,
             self.model_weights,
             self.install_path,
-            self.model_name,
+            model_name,
         )
 
-    def time_prep_classification(self):
+    def time_prep_classification(self, model_name):
         prep_classification(
             self.trained_model,
             self.model_weights,
             self.install_path,
-            self.model_name,
+            model_name,
             self.n_free_cpus,
         )
 
-    def time_prep_training(self):
+    def time_prep_training(self, model_name):
         prep_training(
             self.n_free_cpus,
             self.trained_model,
             self.model_weights,
             self.install_path,
-            self.model_name,
+            model_name,
         )
+
+
+class PrepTF:
+    def setup(self):
+        n_free_cpus = 2  # TODO: should we parametrise this?
+        self.n_processes = get_num_processes(min_free_cpu_cores=n_free_cpus)
+
+    def time_prep_tensorflow(self):
+        prep_tensorflow(self.n_processes)
